e5bc03f29e52e7b4ba26399a55a36cfdeeebbee5
Mark <markzero27@gmail.com>
2019-09-28 04:43:30 +0800

    Initialize Login and Register Modals

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  role = 'instructor';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  changeRole() {
+    if (this.role === 'instructor') {
+      this.role = 'student';
+    } else {
+      this.role = 'instructor';
+    }
   }
-
 }
